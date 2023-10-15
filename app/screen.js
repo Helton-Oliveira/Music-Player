842c5d41f794ddf0cb111d117cd1ea6c40db7200
Helton Oliveira <126279624+Helton-Oliveira@users.noreply.github.com>
2023-10-15 12:49:23 -0300

    feat: adding music progression bar

--- app/screen.js
+++ app/screen.js
@@ -2,12 +2,22 @@ const musicCards = document.querySelector('[data-card]');
 const footerElement = document.querySelector('#footer');
 let audio = new Audio('');
 const favoriteArray = JSON.parse(localStorage.getItem('favoriteSong')) || [];
+const favoriteBtn = document.querySelector('#favorite');
+const btnHome = document.querySelector('#logo');
 
-displayMusclesOnScreen(favoriteArray);
-listenToMusic(favoriteArray);
 
+btnHome.addEventListener('click', () => {
+    window.location.reload(false)
+})
+
+
+favoriteBtn.addEventListener('click', () => {
+    displayMusclesOnScreen(favoriteArray);
+    listenToMusic(favoriteArray);
+})
 
 function displayMusclesOnScreen(data) {
+    musicCards.innerHTML = ''
     data.forEach(card => { 
     musicCards.innerHTML += 
         ` <div class="song__box">
@@ -26,12 +36,17 @@ function listenToMusic(data) {
             song = data.find(item => item.title === e.dataset.img);
             playSong(song);
             footer(data, song);
+              
         })
     })
 };
 
+ 
+
 
 function footer(data, song) {
+    
+    
     footerElement.innerHTML = ` 
         <div class="footer__container">
             <div class="music__card">
@@ -40,23 +55,41 @@ function footer(data, song) {
                         <p class="band">${song.artist.name}</p>
                         <p class="music__name">${song.title}</p>
                 </div>      
-                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-plus" id="plus"></i>
             </div>
 
             <div class="reproduction__container">
                 <div class="controlers">
-                    <i class="fa-solid fa-backward" data-controls="back" id="back"></i>
-                    <i class="fa-solid fa-play closed" data-controls="play" id="play"></i>
-                    <i class="fa-solid fa-pause" data-controls="pause" id="pause"></i>
-                    <i class="fa-solid fa-forward" data-controls="next" id="next"></i>
+                    <div class="controlers__container">
+                        <div class="controlers">
+                            <div class="controls">
+                                <i class="fa-solid fa-backward" data-controls="back" id="back"></i>
+                                <i class="fa-solid fa-play closed" data-controls="play" id="play"></i>
+                                <i class="fa-solid fa-pause" data-controls="pause" id="pause"></i>
+                                <i class="fa-solid fa-forward" data-controls="next" id="next"></i>
+                        </div>
+                        <progress value="0" max="${29.0}" class="reproduction-bar" id="progressBar"></progress>
+                    </div>
+                    <i class="fa-solid fa-volume-xmark volume closed" id="mute" data-controls="muted"></i>
+                    <i class="fa-solid fa-volume-low volume" data-controls="low" id="low"></i>
+                    <progress value="${audio.volume}" max="1" class="volume_bar" id="progress__volume"></progress>
+                    <i class="fa-solid fa-volume-high volume" data-controls="high" id="high"></i>
                 </div>
             </div> 
         </div> `
         controllers(data, song);
+        favoriteMusic(song);  
+        setInterval(verifyReproduction, 1000);
 };
 
 function playSong(song) {
     audio = new Audio(`${song.preview}`);
     audio.play();
 };
+
+function verifyReproduction() {
+    reproductionBar = document.querySelector('#progressBar');
+    return reproductionBar.setAttribute('value', audio.currentTime);
+}
+
 
